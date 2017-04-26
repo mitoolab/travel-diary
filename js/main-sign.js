d2aaa3e230bd5b40b55e464cf69f10ef2f8dafe9
mitoo <mitoolab@gmail.com>
2017-04-26 14:24:10 +0900

    통신 코드 추가

---
+++ js/main-sign.js
@@ -0,0 +1,55 @@
+// 회원가입
+var button_sign_up = document.querySelector(".button-sign-up");
+
+button_sign_up.onclick = function(e) {
+  var sign_up_email = document.querySelector(".input-email-sign-up");
+  // sign_up_email.value
+  // console.log(sign_up_email.value);
+  var sign_up_password = document.querySelector(".input-password-sign-up");
+
+  // 통신
+  axios.post('/api/users/', {
+    "username": sign_up_email,
+    "password": sign_up_password,
+    "user_type": "NORMAL"
+  })
+  // 성공
+  .then(function (response) {
+    // console.log(response);
+    alert("회원가입이 완료되었습니다.")
+  })
+  // 실패
+  .catch(function (error) {
+    // console.log(error);
+    alert("다시 작성하여 주십시오.")
+  });
+
+};
+
+// 로그인
+var button_sign_in = document.querySelector(".button-sign-in");
+
+button_sign_up.onclick = function(e) {
+  var sign_in_email = document.querySelector(".input-email-sign-in");
+  // sign_up_email.value
+  // console.log(sign_up_email.value);
+  var sign_in_password = document.querySelector(".input-password-sign-in");
+
+  // 통신
+  axios.post('/user/login/', {
+    "username": sign_up_email,
+    "password": sign_up_password,
+    "user_type": "NORMAL"
+  })
+  // 성공
+  .then(function (response) {
+    // console.log(response);
+    alert("로그인 되었습니다.")
+  })
+  // 실패
+  .catch(function (error) {
+    // console.log(error);
+    alert("이메일 또는 비밀번호를 확인하여 주십시오.")
+  });
+
+};
