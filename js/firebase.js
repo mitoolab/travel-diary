2733c2984f91897fef4cb25d705a1f5a2ac3d1c7
mitoo <mitoolab@gmail.com>
2017-07-09 12:49:44 +0900

    파이어베이스 설정 중

---
+++ js/firebase.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyAK8xCBpHusr4Mj4mXBDu31Qfu25MaG22o",
+    authDomain: "record-3209b.firebaseapp.com",
+    databaseURL: "https://record-3209b.firebaseio.com",
+    projectId: "record-3209b",
+    storageBucket: "record-3209b.appspot.com",
+    messagingSenderId: "388861185550"
+  };
+  firebase.initializeApp(config);
